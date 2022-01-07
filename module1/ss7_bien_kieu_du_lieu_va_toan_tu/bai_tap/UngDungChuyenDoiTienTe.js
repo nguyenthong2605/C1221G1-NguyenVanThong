3cd98c8d67f78d73c35562363de34111d6d644e7
unknown <you@example.com>
2022-01-07 16:54:29 +0700

    html

--- module1/ss7_bien_kieu_du_lieu_va_toan_tu/bai_tap/UngDungChuyenDoiTienTe.js
+++ module1/ss7_bien_kieu_du_lieu_va_toan_tu/bai_tap/UngDungChuyenDoiTienTe.js
@@ -1,16 +1,16 @@
 function tinhToan() {
 
     let tien = document.getElementById('tien').value;
-    let ln1 = document.getElementById('ln1').value;
-    let ln2 = document.getElementById('ln2').value;
+    let a = document.getElementById('a').value;
+    let b = document.getElementById('b').value;
     let vnd1 = document.getElementById('VND1').value;
     let vnd2 = document.getElementById('VND2').value;
     let usd1 = document.getElementById('USD1').value;
     let usd2 = document.getElementById('USD2').value;
 
-    if(ln1 == vnd1 && ln2 == usd2) {
+    if(a == vnd1 && b == usd2) {
         tien = tien / 23000
-    } else if ((ln1 == vnd1 && ln2 == vnd2) || (ln1 == usd1 && ln2 == usd2)){
+    } else if ((a == vnd1 && b == vnd2) || (a == usd1 && b == usd2)){
         tien = tien
     }
     else {
